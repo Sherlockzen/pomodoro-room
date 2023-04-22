7b469b812beb983a4b9a0da087353a69bc1deee7
Hewerton <hewertonlourenco@gmail.com>
2023-04-21 23:02:29 -0300

    Feat: install and test daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
     <div className=' bg-slate-500 h-screen w-screen flex flex-col justify-center items-center'>
       <div>
         <h1>Temos aqui algo pra colocar</h1>
+        <button 
+        onClick={ () => setCount(count + 1)}
+        className="btn btn-accent">Contador: {count}</button>
       </div>
     </div>
 
