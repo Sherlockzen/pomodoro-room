cc60351a6075d0b98a8db10b5708138ac5bfb540
Hewerton <hewertonlourenco@gmail.com>
2023-04-22 22:09:45 -0300

    Feat: design? really?

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,9 @@ function App() {
 
     <div className=' bg-slate-500 h-screen w-screen flex flex-col justify-center items-center'>
       <div className=' bg-yellow-300 h-[calc(100vh-80px)] max-w-md w-full flex flex-col items-center justify-center border-2 border-red-600'>
-        <h1>Temos aqui algo pra colocar</h1>
-        <button 
-        onClick={ () => setCount(count + 1)}
-        className="btn btn-accent">Contador: {count}</button>
+        <div className=' m-2 w-2/3 bg-green-300'>
+          algo
+        </div>
       </div>
     </div>
 
