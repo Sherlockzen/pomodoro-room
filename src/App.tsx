d09812a3c3badc34ea425e0c5651c4c04dbb71ab
Hewerton <hewertonlourenco@gmail.com>
2023-04-22 22:05:12 -0300

    Feat: testing somme estructure design

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
 
     <div className=' bg-slate-500 h-screen w-screen flex flex-col justify-center items-center'>
-      <div>
+      <div className=' bg-yellow-300 h-[calc(100vh-80px)] max-w-md w-full flex flex-col items-center justify-center border-2 border-red-600'>
         <h1>Temos aqui algo pra colocar</h1>
         <button 
         onClick={ () => setCount(count + 1)}
