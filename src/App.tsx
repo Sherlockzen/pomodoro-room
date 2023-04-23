fc11ab3c8154adeb2cacfadf1c13eb8da1cb0502
Hewerton <hewertonlourenco@gmail.com>
2023-04-22 22:17:35 -0300

    Feat: Nothing to see here...

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-
-    <div className=' bg-slate-500 h-screen w-screen flex flex-col justify-center items-center'>
-      <div className=' bg-yellow-300 h-[calc(100vh-80px)] max-w-md w-full flex flex-col items-center justify-center border-2 border-red-600'>
-        <div className=' m-2 w-2/3 bg-green-300'>
-          algo
-        </div>
+    <div className=" bg-slate-500 h-screen w-screen flex flex-col justify-center items-center">
+      <div className=" bg-yellow-300 h-[calc(100vh-80px)] max-w-md w-full flex flex-col items-center justify-center border-2 border-red-600">
+        <div className=" m-2 w-2/3 bg-green-300">algo</div>
       </div>
+      
     </div>
-
-  )
+  );
 }
 
 export default App
